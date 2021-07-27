e1273de56ae0de2b08ee6113fbfbdda0a2157428
Markus Schwer <markus_schwer@web.de>
2021-07-27 23:03:02 +0200

    continue with index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from "react";
 import { graphql, PageProps, useStaticQuery } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 import Layout from "../components/layout";
 import MainContent from "../components/main-content";
@@ -16,50 +17,59 @@ import "./index.scss";
 const IndexPage: FunctionComponent<PageProps> = ({ location }) => {
   const data = useStaticQuery(graphql`
     query {
-      file(relativePath: { eq: "front-page-1.png" }) {
+      frontPageImage: file(relativePath: { eq: "front-page-1.png" }) {
         childImageSharp {
           gatsbyImageData(placeholder: BLURRED, layout: FULL_WIDTH)
+        }
+      }
+      ideaImage: file(relativePath: { eq: "idea-offene-werkstatt.jpg" }) {
+        childImageSharp {
+          gatsbyImageData(placeholder: BLURRED)
         }
       }
     }
   `);
 
+  const ideaImage = getImage(data.ideaImage)!;
+
   return (
-    <Layout location={location} image={data.file} imageAlt="">
+    <Layout location={location} image={data.frontPageImage} imageAlt="">
       <MainContent location={location}>
-        <h1 className="front-page-header">
-          Das Schülerforschungszentrum der Hochschule Aalen
-        </h1>
-        <p className="front-page-text">
-          Interessierst du dich dafür, wie Dinge funktionieren? Tüftelst,
-          experimentierst und forscht du gerne? Dann bist du bei uns richtig!
-        </p>
-        <div className="icon-bubble-container">
-          <IconBubble
-            icon={<IconMicroscope />}
-            text="Mitmachen"
-            color="tertiary"
-            to="#"
-          />
-          <IconBubble
-            icon={<IconEinstein />}
-            text="Projekte"
-            color="primary"
-            to="#"
-          />
-          <IconBubble
-            icon={<IconPlanet />}
-            text="Workshops"
-            color="secondary"
-            to="#"
-          />
-          <IconBubble
-            icon={<IconLightBulb />}
-            text="Aktuelles"
-            color="tertiary"
-            to="#"
-          />
-        </div>
+        <section>
+          <h1 className="front-page-header">
+            Das Schülerforschungszentrum der Hochschule Aalen
+          </h1>
+          <p className="front-page-text">
+            Interessierst du dich dafür, wie Dinge funktionieren? Tüftelst,
+            experimentierst und forscht du gerne? Dann bist du bei uns richtig!
+          </p>
+          <div className="icon-bubble-container">
+            <IconBubble
+              icon={<IconMicroscope />}
+              text="Mitmachen"
+              color="tertiary"
+              to="#"
+            />
+            <IconBubble
+              icon={<IconEinstein />}
+              text="Projekte"
+              color="primary"
+              to="#"
+            />
+            <IconBubble
+              icon={<IconPlanet />}
+              text="Workshops"
+              color="secondary"
+              to="#"
+            />
+            <IconBubble
+              icon={<IconLightBulb />}
+              text="Aktuelles"
+              color="tertiary"
+              to="#"
+            />
+          </div>
+        </section>
 
         <FullWidthSection className="colored-section">
           <h1>Die Idee</h1>
@@ -70,19 +80,42 @@ const IndexPage: FunctionComponent<PageProps> = ({ location }) => {
             Forschung.
           </h2>
           <hr className="inverted" />
+          <div className="row">
+            <div className="col-6">
+              <GatsbyImage image={ideaImage} alt="offene werkstatt" />
+            </div>
+            <p className="col-6" style={{ margin: 0, lineHeight: "1.5625" }}>
+              Bei uns geht die Beschäftigung mit MINT weit über den
+              Schulunterricht hinaus und die Jugendlichen gehen selbstständig
+              und eigenverantwortlich einer eigenen Forschungs-fragestellung auf
+              den Grund. Sie entwickeln Problemstellungen und Hypothesen, planen
+              Experimente und werten sie aus, erarbeiten Modelle, finden
+              Erklärungen und hinterfragen diese kritisch.
+              <br />
+              <br />
+              Die Jugendlichen arbeiten über längere Zeit in Teams zusammen und
+              werden von kompetenten Mentoren unterstützt.
+              <br />
+              <br />
+              Gerade für Jüngere oder Neueinsteiger gibt es die Möglichkeit, bei
+              bestehenden Teams mitzuarbeiten, die sich bereits mit einem Thema
+              auseinandersetzen; dadurch kommen sie leichter ins selbständige
+              Forschen.
+            </p>
+          </div>
         </FullWidthSection>
 
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-          takimata sanctus est Lorem ipsum dolor sit amet.
-        </p>
+        <section>
+          <h1>Unser Team</h1>
+          <h2>
+            Unser Team besteht aus Schülern, Azubis, Lehrern, wissenschaftlichen Mitarbeitern und
+          manchmal auch Studierenden, die ehrenamtlich für das SFZ tätig sind. Wir können euch
+          in den Bereichen Informatik, 3D-Druck, Holz-/Metallbearbeitung, Physik und noch einiges
+          mehr Unterstützung bieten. Wenn eure Fragen über unser Wissen und Können hinausge-
+          hen, können wir Experten vermitteln.
+          </h2>
+          <hr />
+        </section>
       </MainContent>
     </Layout>
   );
