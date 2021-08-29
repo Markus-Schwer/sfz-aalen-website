005dcc785f6a8945b3b6cfa24151ca50cfd84e5f
Markus Schwer <markus_schwer@web.de>
2021-08-29 14:31:26 +0200

    only use typescript for gatsby node

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,2 +1,78 @@
-const { generateConfig } = require('gatsby-plugin-ts-config');
-module.exports = generateConfig();
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "SFZ Aalen",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-transformer-remark",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+    "gatsby-plugin-sass",
+    "gatsby-plugin-offline",
+    {
+      resolve: 'gatsby-plugin-typescript',
+      options: {
+        isTSX: true,
+        allExtensions: true,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /\.svg$/
+        }
+      }
+    },
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: "./content/pages/",
+        name: "pages"
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "static",
+        path: "./static",
+      },
+      __key: "static",
+    },
+    "gatsby-transformer-json",
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: "./src/cms/netlify-cms.ts",
+      },
+    },
+    "gatsby-plugin-netlify" // make sure to keep it last in the array
+  ],
+  pathPrefix: "/sfz-aalen-website" // only for GitHub Pages
+};
--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,21 +1,25 @@
 import { GatsbyNode } from "gatsby";
 import { resolve } from "path";
+import { ImageDataLike } from "gatsby-plugin-image";
 
-export interface Node {
-  title: string
-  path: string
-  breadcrumbs: string[]
+export interface PageData {
+  title: string;
+  path: string;
+  thumbnails?: ImageDataLike[];
+  thumbnailUrls?: string[];
+  previewThumbnails?: string[];
+  breadcrumbs: string[];
   motto: {
-    text: string
-    size: string
-    color: string
-  }
+    text: string;
+    size: string;
+    color: string;
+  }[];
 }
 
 interface QueryResult {
   allPagesJson: {
     edges: {
-      node: Node;
+      node: PageData;
     }[];
   };
 }
@@ -23,11 +27,14 @@ interface QueryResult {
 /**
  * Gatsby exposes interfaces for every lifecycle hook
  */
-export const createPages: GatsbyNode['createPages'] = async ({ graphql, actions }) => {
-  const { createPage } = actions
+export const createPages: GatsbyNode["createPages"] = async ({
+  graphql,
+  actions,
+}) => {
+  const { createPage } = actions;
 
   // Ensure the path now points to TSX template
-  const blogPostTemplate = resolve(`./src/templates/page-template.tsx`)
+  const blogPostTemplate = resolve(`./src/templates/page-template.tsx`);
 
   /**
    * Pass the query structure generic for complete type-check coverage
@@ -49,14 +56,14 @@ export const createPages: GatsbyNode['createPages'] = async ({ graphql, actions 
         }
       }
     }
-  `)
+  `);
 
   if (result.errors) {
-    throw result.errors
+    throw result.errors;
   }
 
   if (!result.data) {
-    throw new Error('ERROR: Could not fetch pages on build');
+    throw new Error("ERROR: Could not fetch pages on build");
   }
 
   const pages = result.data.allPagesJson.edges;
@@ -66,6 +73,27 @@ export const createPages: GatsbyNode['createPages'] = async ({ graphql, actions 
       path: post.node.path,
       component: blogPostTemplate,
       context: {},
-    })
+    });
   });
-}
+};
+
+export const createResolvers: GatsbyNode["createResolvers"] = ({createResolvers}) => {
+  createResolvers({
+    PagesJson: {
+      thumbnails: {
+        type: "[File]!",
+        resolve(source: any, args: any, context: any, info: any) {
+          return context.nodeModel.runQuery({
+            query: {
+              filter: {
+                relativePath: { in: source.thumbnailUrls },
+              },
+            },
+            type: "File",
+            firstOnly: false
+          });
+        },
+      },
+    },
+  });
+};
