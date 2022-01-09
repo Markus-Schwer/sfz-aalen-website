546b288656e6133c616dc2b43d54b67382a81f93
Markus Schwer <markus.schwer@protonmail.com>
2022-01-09 15:54:33 +0100

    fix broken cms preview (caused by seo)

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from "react";
 import { Helmet } from "react-helmet";
 import { useLocation } from "@reach/router";
-import { useStaticQuery, graphql } from "gatsby";
+import { StaticQuery, graphql } from "gatsby";
 
 type SEOProps = {
   title?: string;
@@ -20,126 +20,135 @@ const SEO: FunctionComponent<SEOProps> = ({
   article,
 }) => {
   const { pathname } = useLocation();
-  const { site } = useStaticQuery(query);
 
-  const {
-    defaultTitle,
-    titleTemplate,
-    defaultDescription,
-    headline,
-    siteUrl,
-    defaultImage,
-    organization,
-    siteLanguage,
-  } = site.siteMetadata;
-
-  const seo = {
-    title: title || defaultTitle,
-    description: description || defaultDescription,
-    image: `${siteUrl}${image || defaultImage}`,
-    url: `${siteUrl}${pathname}`,
-  };
-
-  // schema.org in JSONLD format
-  // https://developers.google.com/search/docs/guides/intro-structured-data
-
-  const schemaOrgWebPage = {
-    "@context": "http://schema.org",
-    "@type": "WebPage",
-    url: siteUrl,
-    headline,
-    inLanguage: siteLanguage,
-    mainEntityOfPage: `${siteUrl}/home`,
-    description: defaultDescription,
-    name: defaultTitle,
-    image: {
-      "@type": "ImageObject",
-      url: `${siteUrl}${defaultImage}`,
-    },
-  };
-
-  // Initial breadcrumb list
-  const itemListElement = [
-    {
-      "@type": "ListItem",
-      item: {
-        "@id": `${siteUrl}/home`,
-        name: "Homepage",
-      },
-      position: 1,
-    },
-  ];
-
-  let schemaArticle = null;
-
-  if (article) {
-    schemaArticle = {
-      "@context": "http://schema.org",
-      "@type": "BlogPosting",
-      publisher: {
-        "@type": "Organization",
-        name: organization,
-        logo: {
-          "@type": "ImageObject",
-          url: `${siteUrl}${defaultImage}`,
-        },
-      },
-      datePublished: article.first_publication_date,
-      dateModified: article.last_publication_date,
-      description: seo.description,
-      headline: title,
-      inLanguage: siteLanguage,
-      url: seo.url,
-      name: seo.title,
-      image: {
-        "@type": "ImageObject",
-        url: seo.image,
-      },
-      mainEntityOfPage: seo.url,
-    };
-
-    // Push current article into breadcrumb list
-    itemListElement.push({
-      "@type": "ListItem",
-      item: {
-        "@id": seo.url,
-        name: seo.title,
-      },
-      position: 2,
-    });
-  }
-
-  const breadcrumb = {
-    "@context": "http://schema.org",
-    "@type": "BreadcrumbList",
-    itemListElement,
-  };
-
+  // using StaticQuery component here, because the hook breaks when it is used with netlify cms
   return (
-    <Helmet title={seo.title} titleTemplate={titleTemplate}>
-      <html lang={siteLanguage} />
-      <meta charSet="utf-8" />
-      <meta name="description" content={seo.description} />
-      <meta name="image" content={seo.image} />
-      {seo.url && <meta property="og:url" content={seo.url} />}
-      {article && <meta property="og:type" content="article" />}
-      {seo.title && <meta property="og:title" content={seo.title} />}
-      {seo.description && (
-        <meta property="og:description" content={seo.description} />
-      )}
-      {seo.image && <meta property="og:image" content={seo.image} />}
-      {/* Insert schema.org data conditionally (webpage/article) + everytime (breadcrumbs) */}
-      {article == undefined ? (
-        <script type="application/ld+json">
-          {JSON.stringify(schemaOrgWebPage)}
-        </script>
-      ) : (
-          <script type="application/ld+json">
-            {JSON.stringify(schemaArticle)}
-          </script>
-      )}
-      <script type="application/ld+json">{JSON.stringify(breadcrumb)}</script>
-    </Helmet>
+    <StaticQuery
+      query={query}
+      render={({ site }) => {
+        const {
+          defaultTitle,
+          titleTemplate,
+          defaultDescription,
+          headline,
+          siteUrl,
+          defaultImage,
+          organization,
+          siteLanguage,
+        } = site.siteMetadata;
+
+        const seo = {
+          title: title || defaultTitle,
+          description: description || defaultDescription,
+          image: `${siteUrl}${image || defaultImage}`,
+          url: `${siteUrl}${pathname}`,
+        };
+
+        // schema.org in JSONLD format
+        // https://developers.google.com/search/docs/guides/intro-structured-data
+
+        const schemaOrgWebPage = {
+          "@context": "http://schema.org",
+          "@type": "WebPage",
+          url: siteUrl,
+          headline,
+          inLanguage: siteLanguage,
+          mainEntityOfPage: `${siteUrl}/home`,
+          description: defaultDescription,
+          name: defaultTitle,
+          image: {
+            "@type": "ImageObject",
+            url: `${siteUrl}${defaultImage}`,
+          },
+        };
+
+        // Initial breadcrumb list
+        const itemListElement = [
+          {
+            "@type": "ListItem",
+            item: {
+              "@id": `${siteUrl}/home`,
+              name: "Homepage",
+            },
+            position: 1,
+          },
+        ];
+
+        let schemaArticle = null;
+
+        if (article) {
+          schemaArticle = {
+            "@context": "http://schema.org",
+            "@type": "BlogPosting",
+            publisher: {
+              "@type": "Organization",
+              name: organization,
+              logo: {
+                "@type": "ImageObject",
+                url: `${siteUrl}${defaultImage}`,
+              },
+            },
+            datePublished: article.first_publication_date,
+            dateModified: article.last_publication_date,
+            description: seo.description,
+            headline: title,
+            inLanguage: siteLanguage,
+            url: seo.url,
+            name: seo.title,
+            image: {
+              "@type": "ImageObject",
+              url: seo.image,
+            },
+            mainEntityOfPage: seo.url,
+          };
+
+          // Push current article into breadcrumb list
+          itemListElement.push({
+            "@type": "ListItem",
+            item: {
+              "@id": seo.url,
+              name: seo.title,
+            },
+            position: 2,
+          });
+        }
+
+        const breadcrumb = {
+          "@context": "http://schema.org",
+          "@type": "BreadcrumbList",
+          itemListElement,
+        };
+
+        return (
+          <Helmet title={seo.title} titleTemplate={titleTemplate}>
+            <html lang={siteLanguage} />
+            <meta charSet="utf-8" />
+            <meta name="description" content={seo.description} />
+            <meta name="image" content={seo.image} />
+            {seo.url && <meta property="og:url" content={seo.url} />}
+            {article && <meta property="og:type" content="article" />}
+            {seo.title && <meta property="og:title" content={seo.title} />}
+            {seo.description && (
+              <meta property="og:description" content={seo.description} />
+            )}
+            {seo.image && <meta property="og:image" content={seo.image} />}
+            {/* Insert schema.org data conditionally (webpage/article) + everytime (breadcrumbs) */}
+            {article == undefined ? (
+              <script type="application/ld+json">
+                {JSON.stringify(schemaOrgWebPage)}
+              </script>
+            ) : (
+              <script type="application/ld+json">
+                {JSON.stringify(schemaArticle)}
+              </script>
+            )}
+            <script type="application/ld+json">
+              {JSON.stringify(breadcrumb)}
+            </script>
+          </Helmet>
+        );
+      }}
+    />
   );
 };
 
